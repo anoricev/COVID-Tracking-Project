9b77fe6cf6fb102b01a3b024f844807e09f406a3
Veronica Lin <veronicalin730@gmail.com>
2025-11-20 19:05:52 -0600

    update airflow & upload powerpoint

--- airflow/dags/covid_pipeline.py
+++ airflow/dags/covid_pipeline.py
@@ -5,7 +5,8 @@ from airflow.operators.empty import EmptyOperator
 import os
 
 PROJECT_DIR = os.environ.get("COVID_PROJECT_DIR", "/opt/airflow/COVID-Tracking-Project")
-VENV_ACT = f"source {PROJECT_DIR}/.venv/bin/activate"
+VENV_ACT = "source .venv/bin/activate"
+DASHBOARD_URL = "https://veronica-lin.shinyapps.io/covid-19-dashboard/"
 
 with DAG(
     dag_id="covid_tracking_pipeline",
@@ -17,14 +18,32 @@ with DAG(
 
     # Fetch new data from API
     fetch_data = BashOperator(
-        task_id="fetch_data",
-        bash_command=f"cd {PROJECT_DIR} && {VENV_ACT} && bash src/fetch_covid_data.sh",
+    task_id="fetch_data",
+    bash_command=f"""
+        cd {PROJECT_DIR}
+        {VENV_ACT}
+        bash src/fetch_covid_data.sh
+    """,
     )
 
     # Clean and transform processed tables
     clean_data = BashOperator(
-        task_id="clean_data",
-        bash_command=f"cd {PROJECT_DIR} && {VENV_ACT} && python src/clean_data.py --raw-dir data/raw --out-dir data/processed",
+    task_id="clean_data",
+    bash_command=f"""
+        cd {PROJECT_DIR}
+        {VENV_ACT}
+        python src/clean_data.py --raw-dir data/raw --out-dir data/processed
+    """,
+    )
+
+    # Build database
+    build_db = BashOperator(
+    task_id="build_database",
+    bash_command=f"""
+        cd {PROJECT_DIR}
+        {VENV_ACT}
+        python -u src/build_database.py --in data/processed --out data/covid_clean.db
+    """,
     )
 
     # dummy operator for notebook analysis
@@ -37,11 +56,14 @@ with DAG(
         task_id="State_Level_Analysis",
     )
 
-    # Run shiny app
-    run_shiny = BashOperator(
-        task_id="run_shiny_app",
-        bash_command=f"cd {PROJECT_DIR} && {VENV_ACT} && shiny run --reload src/app.py",
-        trigger_rule="all_done",
+    # Check the availability of the deployed Shiny dashboard
+    check_shiny = BashOperator(
+    task_id="check_shiny_dashboard",
+    bash_command=f"""
+        echo "Pinging Shiny dashboard at {DASHBOARD_URL}"
+        curl -sSf {DASHBOARD_URL} >/dev/null
+    """,
+    trigger_rule="all_done",
     )
 
-    fetch_data >> clean_data >> National_Level_Analysis >> State_Level_Analysis >>run_shiny
+    fetch_data >> clean_data >> build_db >> [National_Level_Analysis, State_Level_Analysis, check_shiny]
